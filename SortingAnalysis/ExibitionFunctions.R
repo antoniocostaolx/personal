773eae59ba13db9eb33f5568128fe24972522030
rodrigo-caro <rodrigo.caro@olx.com>
2017-01-05 18:18:58 +0000

    Included Loads

--- SortingAnalysis/ExibitionFunctions.R
+++ SortingAnalysis/ExibitionFunctions.R
@@ -1,3 +1,8 @@
+#Load the file containing the DataFrame with Autovit data
+load("ExibitionAutovit.RData")
+#Load the file containing DataFrame with Otomot data
+load("ExibitionOtomoto.RData")
+
 TotalExibitionAutovit <- function(input){
   KMSorting <-  sum (TotalAutovit[
     TotalAutovit$Date >= input$date_range[1] & 
